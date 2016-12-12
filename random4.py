0ef0789a576333f17422b603c104b196228dfec6
NikoliaSHA <digital2007@list.ru>
2016-12-12 09:08:13 +0500

    begin

---
+++ random4.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @Date    : 05-Oct-2016 12:54 AM
+# @Authors : Nikolia
+# @Python  : 3
+
+# выбор случайного числа из диапазона
+from random import randint
+
+print(randint(198, 312))
